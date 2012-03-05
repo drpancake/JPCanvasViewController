0c2420468d0266e4b83d9da6a32f4827628a6cb3
James Potter <james.potter@gmail.com>
2012-03-05 23:09:03 +0000

    First pass

---
+++ JPCanvasViewController.h
@@ -0,0 +1,47 @@
+//
+//  JPCanvasViewController.h
+//  Created by James Potter on 05/03/2012.
+//
+
+extern NSString *const JPCANVAS_BASE_HTML;
+
+/*
+  OS X
+*/
+
+#if (TARGET_OS_MAC && !TARGET_OS_IPHONE)
+
+#import <Cocoa/Cocoa.h>
+#import <WebKit/WebKit.h>
+
+@interface JPCanvasViewController : NSViewController {
+    @private
+    WebView *_webView;
+}
+
+@property (nonatomic, strong, readonly) WebView *webView;
+
+/*
+  iOS
+*/
+
+#elif TARGET_OS_IPHONE
+
+#import <UIKit/UIKit.h>
+
+@interface JPCanvasViewController : UIViewController<UIWebViewDelegate> {
+    @private
+    UIWebView *_webView;
+}
+
+@property (nonatomic, strong, readonly) UIWebView *webView;
+
+#endif
+
+/*
+  Shared methods
+*/
+
+- (NSString *)execute:(NSString *)js;
+
+@end
---
+++ JPCanvasViewController.m
@@ -0,0 +1,100 @@
+//
+//  JPCanvasViewController.m
+//  Created by James Potter on 05/03/2012.
+//
+
+#import "JPCanvasViewController.h"
+
+/*
+  Shared HTML for a canvas spanning its entire WebView/UIWebView
+  with no margins
+*/
+NSString *const JPCANVAS_BASE_HTML = 
+    @"<html><body style='margin: 0'>"
+    "<canvas style='width: 100%; height=100%' id='canvas'></canvas>"
+    "<script type='text/javascript'>"
+    "window.canvas = document.getElementById('canvas');"
+    "window.context = window.canvas.getContext('2d');"
+    "</script>"
+    "</body></html>";
+
+@implementation JPCanvasViewController
+
+@synthesize webView=_webView;
+
+/*
+  OS X specific methods for preparing a WebView
+*/
+
+#if (TARGET_OS_MAC && !TARGET_OS_IPHONE)
+
+- (WebView *)webView
+{
+    if (_webView == nil) {
+        // Dummy size for now (TODO: figure out how to inherit sizing)
+        _webView = [[WebView alloc] initWithFrame:CGRectMake(0, 0, 500, 500)];
+    }
+    return _webView;
+}
+
+- (void)loadView
+{
+    // Load nib
+    [super loadView];
+    
+    [self.webView setFrameLoadDelegate:self];
+    
+    WebFrame *webFrame = [self.webView mainFrame];
+    [webFrame loadHTMLString:JPCANVAS_BASE_HTML baseURL:[NSURL URLWithString:@""]];
+    
+    [self setView:self.webView];
+}
+
+// WebFrameLoadDelegate
+- (void)webView:(WebView *)sender didFinishLoadForFrame:(WebFrame *)frame
+{
+    NSString *js = @"window.context.fillStyle='red'; window.context.fillRect(50, 50, 100, 100);";
+    [self execute:js];
+}
+
+/*
+  iOS-specific methods for preparing a UIWebView
+*/
+
+#elif TARGET_OS_IPHONE
+
+- (UIWebView *)webView
+{
+    if (_webView == nil) {        
+        _webView = [[UIWebView alloc] initWithFrame:self.parentViewController.view.bounds];
+        _webView.delegate = self;
+        [_webView loadHTMLString:JPCANVAS_BASE_HTML baseURL:[NSURL URLWithString:@""]];
+    }
+    return _webView;
+}
+
+// UIWebViewDelegate
+- (void)webViewDidFinishLoad:(UIWebView *)theWebView
+{
+    NSString *js = @"window.context.fillStyle='red'; window.context.fillRect(50, 50, 100, 100);";
+    [self execute:js];
+}
+
+- (UIView *)view
+{
+    return self.webView;
+}
+
+#endif
+
+/*
+  Past this point only stringByEvaluatingJavaScriptFromString: is needed, which is
+  provided in both WebView and UIWebView, so we can just refer to self.webView
+*/
+
+- (NSString *)execute:(NSString *)js
+{
+    return [self.webView stringByEvaluatingJavaScriptFromString:js];
+}
+
+@end
